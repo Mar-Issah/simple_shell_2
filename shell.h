1822b880aab520e2ca1fd7d31b088f522502424c
Marsiya Issah <masy370@gmail.com>
2023-04-18 19:16:53 -0400

    update shell.h file

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#define PRINT(char) (write(STDOUT_FILENO, char, _strlen(char)))
+
+// string functions prop
+int _strlen(char *str);
+
+#endif /* SHELL_H */
