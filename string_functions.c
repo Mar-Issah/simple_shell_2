a1f6452a8b6a3bc44d8e044093b6aee40d646ff0
Marsiya Issah <masy370@gmail.com>
2023-04-20 23:33:18 -0400

    Update string_functions.c

--- string_functions.c
+++ string_functions.c
@@ -12,3 +12,43 @@ int _strlen(char *str)
 		;
 	return (len);
 }
+
+/**
+ * _strcpy - copie a string from source to destination
+ * @source: the string source
+ * @destination: the string destination
+ *
+ * Return: the pointer to destination
+ */
+char *_strcpy(char *destination, char *source)
+{
+	int i;
+
+	for (i = 0; source[i] != '\0'; i++)
+	{
+		destination[i] = source[i];
+	}
+	destination[i] = '\0';
+	return (destination);
+}
+
+
+/**
+ * _strdup - dupicates string
+ * @str: the given string
+ *
+ * Return: (Success) a pointer to the duplicated string
+ * ------- (Fail) return a null pointer
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+
+	if (str == NULL)
+		return (NULL);
+	duplicate = malloc(_strlen(str) + 1);
+	if (duplicate == NULL)
+		return (NULL);
+	_strcpy(duplicate, str);
+	return (duplicate);
+}
