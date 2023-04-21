83366a27b9755ee49535739a404324bc30a7ff83
Marsiya Issah <masy370@gmail.com>
2023-04-20 23:44:38 -0400

    Update util.c

--- util.c
+++ util.c
@@ -25,36 +25,99 @@ char *read_command()
     return line;
 }
 
+char *search_command(char *command)
+{
+    char *path_env = _getenv("PATH");
+    char *path_env_copy = _strdup(path_env);
+    char *path = strtok(path_env_copy, ":");
+    char *full_path = malloc(PATH_MAX);
+    int path_len, command_len;
+
+
+    while (path) {
+         path_len = _strlen(path);
+        _memcpy(full_path, path, path_len);
+        full_path[path_len] = '/';
+        command_len = _strlen(command);
+        _memcpy(full_path + path_len + 1, command, command_len);
+        full_path[path_len + 1 + command_len] = '\0';
+        
+        
+        if (access(full_path, X_OK) == 0)
+        {
+            free(path_env_copy);
+            return full_path;
+        }
+        path = strtok(NULL, ":");
+    }
+    free(path_env_copy);
+    free(full_path);
+    
+    return (NULL);
+}
+
+
 /**
  * execute_command - function to execute the command
  * @command: command to be executed
  * Fail: Exit
  */
-void execute_command(char *command)
+void execute_command(char *command_line)
 {
     pid_t pid;
     int status;
+    char *token;
+    char **args;
+    int arg_count = 0;
+    char *full_path;
+
+    args = malloc(sizeof(char *) * 64);
+    if (!args)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    
+    token = strtok(command_line, " \t");
+    while (token) {
+        args[arg_count] = token;
+        arg_count++;
+        token = strtok(NULL, " \t");
+    }
+    args[arg_count] = NULL;
+
+    full_path = search_command(args[0]);
+    if (!full_path) {
+        /* fprintf(stderr, "%s: command not found\n", args[0]);*/
+        print_error(STDERR_FILENO, args[0]);
+        free(args);
+        return;
+    }
 
     pid = fork();
-    if (pid == -1)
-    {
+    if (pid == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
-    } else if (pid == 0) {
+    } 
+    else if (pid == 0)
+    {
         /*-- Child process--*/
-        if (execlp(command, command, NULL) == -1)
+        if (execve(full_path, args, environ) == -1)
         {
-            print_error(STDERR_FILENO, command);
+            /* fprintf(stderr, "%s: command execution failed\n", args[0]);*/
+            print_error(STDERR_FILENO, args[0]);
             exit(EXIT_FAILURE);
         }
     }
     else
     {
         /*-- Parent process--*/
-        do
-        {
+        do {
             waitpid(pid, &status, WUNTRACED);
-        }
-        while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
+
+
+    free(args);
+    free(full_path);
 }
