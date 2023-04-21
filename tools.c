634656c09a270a5d3bd5841ae16540056ea41ee0
Marsiya Issah <masy370@gmail.com>
2023-04-20 23:35:07 -0400

    Update tools.c

--- tools.c
+++ tools.c
@@ -14,3 +14,52 @@ void print_error(int fd, char *command)
     write(fd, command, _strlen(command));
     write(fd, error_msg, _strlen(error_msg));
 }
+
+/**
+ * _getenv - gets the path
+ * @path_name: a pointer to the struct of data
+ *
+ * Return: Success, a positive number
+ * Fail, a negative number
+ */
+char *_getenv(char *path_name)
+{
+	char **environ_cursor, *env_ptr, *name_ptr;
+
+	environ_cursor = environ;
+	while (*environ_cursor)
+	{
+		env_ptr = *environ_cursor;
+		name_ptr = path_name;
+		while (*env_ptr == *name_ptr)
+		{
+			if (*env_ptr == '=')
+				break;
+			env_ptr++;
+			name_ptr++;
+		}
+		if ((*env_ptr == '=') && (*name_ptr == '\0'))
+			return (env_ptr + 1);
+		environ_cursor++;
+	}
+	return (NULL);
+}
+
+/**
+ * _memcpy - cpies memory area
+ * @dest: Destination memory area
+ * @src: Source memory area
+ * @n: Amount of memory byte
+ *
+ * Return: A pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
