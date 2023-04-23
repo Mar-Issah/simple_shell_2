c3c1eb644476bc0f7b52038501d49520240ade35
Marsiya Issah <masy370@gmail.com>
2023-04-23 19:45:44 -0400

    Update string_functions.c

--- string_functions.c
+++ string_functions.c
@@ -52,3 +52,25 @@ char *_strdup(char *str)
 	_strcpy(duplicate, str);
 	return (duplicate);
 }
+
+/**
+  * _strcmp - a function to compare two strings
+  * @s1: the first string
+  * @s2: the second string
+  *
+  * Return: int
+  */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+		break;
+	}
+	return (*s1 - *s2);
+}
