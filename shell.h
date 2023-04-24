70b276cec59c1f46eaa4ae4d335ed067e560062f
Marsiya Issah <masy370@gmail.com>
2023-04-23 22:06:15 -0400

    Update shell.h

--- shell.h
+++ shell.h
@@ -37,5 +37,6 @@ char *_getenv(char *path_name);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *search_command(char *command);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+int _atoi(char *c);
 
 #endif /* SHELL_H */
