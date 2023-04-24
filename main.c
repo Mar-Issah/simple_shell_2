85aa4f464c1fde5076b89754a2ca7d23eaeaf65a
Marsiya Issah <masy370@gmail.com>
2023-04-24 17:50:19 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,35 +5,40 @@
  *
  * Return: (Success) 0 
  */
-int main() {
+int main()
+{
     char *line;
     char *command;
     char **args;
     int arg_count = 0;
     int exit_status = 0;
 
-    while (1) {
+    while (1)
+    {
         line = read_command();
-        command = _strtok(line, "\n");
-      
-        if (command != NULL && _strlen(command) > 0) {
+        if (line != NULL && _strlen(line) > 0)
+        {
             args = malloc(sizeof(char *) * 64);
             arg_count = 0;
-            args[arg_count++] = _strtok(command, " \t");
-            
-             while ((args[arg_count++] = _strtok(NULL, " \t")))
-                 ;
-            
-              if (_strcmp(args[0], "exit") == 0) 
-               {
-                if (args[1] != NULL) {
+            command = _strtok(line, " \t\n");
+            while (command != NULL)
+            {
+                args[arg_count++] = command;
+                command = _strtok(NULL, " \t\n");
+            }
+            args[arg_count] = NULL;
+
+            if (_strcmp(args[0], "exit") == 0)
+            {
+                if (args[1] != NULL)
+                {
                     exit_status = _atoi(args[1]);
                 }
                 free(line);
                 free(args);
                 exit(exit_status);
             }
-            execute_command(command);
+            execute_command(args);
             free(args);
         }
         free(line);
