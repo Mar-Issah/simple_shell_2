98d475fb3cb9128b2cf54e7ad49181676ef81378
Marsiya Issah <masy370@gmail.com>
2023-04-24 20:00:48 -0400

    Update main.c

--- main.c
+++ main.c
@@ -12,42 +12,47 @@ int main()
     char **args;
     int arg_count = 0;
     int exit_status = 0;
+    char **commands;
 
     while (1)
     {
         line = read_command();
-        if (line != NULL && _strlen(line) > 0)
-        {
-            args = malloc(sizeof(char *) * 64);
-            arg_count = 0;
-            command = _strtok(line, " \t\n");
-            while (command != NULL)
-            {
-                args[arg_count++] = command;
-                command = _strtok(NULL, " \t\n");
-            }
-            args[arg_count] = NULL;
+        commands = parse_commands(line);
+        int i = 0;
+        
+        while (commands[i]) {
+            command = _strtok(commands[i], "\n");
 
-            if (_strcmp(args[0], "exit") == 0)
+            if (command != NULL && _strlen(command) > 0)
             {
-                if (args[1] != NULL)
+                args = malloc(sizeof(char *) * 64);
+                arg_count = 0;
+                args[arg_count++] = _strtok(command, " \t");
+                while ((args[arg_count++] = _strtok(NULL, " \t")));
+
+                if (_strcmp(args[0], "exit") == 0) 
                 {
-                    exit_status = _atoi(args[1]);
+                    if (args[1] != NULL)
+                    {
+                        exit_status = _atoi(args[1]);
+                    }
+                    free(line);
+                    free(args);
+                    free(commands);
+                    exit(exit_status);
                 }
-                free(line);
+                 else if (_strcmp(args[0], "cd") == 0)
+                 {
+                    cd_builtin(args);
+                } else {
+                    execute_command(args);
+                }
                 free(args);
-                exit(exit_status);
             }
-             else if (_strcmp(args[0], "cd") == 0)
-             {
-                cd_builtin(args);
-            } else
-             {
-                execute_command(args);
-            }
-            free(args);
+            i++;
         }
         free(line);
+        free(commands);
     }
     return (EXIT_SUCCESS);
 }
