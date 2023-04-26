e61fe35ca09c01548f4e90158ccc54ee4fed4715
Marsiya Issah <masy370@gmail.com>
2023-04-26 10:05:58 -0400

    Update more_tools.c

--- more_tools.c
+++ more_tools.c
@@ -1,7 +1,7 @@
 #include "shell.h"
-/*
+/**
 * cd_builtin - function that implement 'cd'
-*   args - an array of strings containing the command and argument(s)
+* args - an array of strings containing the command and argument(s)
 *
 * Return value: none
 */
@@ -12,7 +12,8 @@ char *old_directory;
 char *home_directory;
 char current_directory[BUFFER_SIZE];
 
-if (args[1] == NULL || _strcmp(args[1], "~") == 0 || _strcmp(args[1], "$HOME") == 0)
+if (args[1] == NULL || _strcmp(args[1], "~") == 0
+|| _strcmp(args[1], "$HOME") == 0)
 {
 home_directory = _getenv("HOME");
 new_directory = home_directory;
@@ -20,24 +21,22 @@ new_directory = home_directory;
 {
 new_directory = _getenv("OLDPWD");
 PRINT(new_directory);
-} else
+}
+else
 {
 new_directory = args[1];
-}
-	
+}	
 old_directory = getcwd(current_directory, sizeof(current_directory));
 if (old_directory == NULL)
 {
 perror("getcwd");
 return;
 }
-
 if (chdir(new_directory) != 0)
 {
 perror("chdir");
 return;
 }
-	
 if (setenv("PWD", new_directory, 1) == -1)
 {
 perror("setenv");
