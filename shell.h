cf86b31d9095dfe1fc0dcf0e6c347b0223f13bb4
Marsiya Issah <masy370@gmail.com>
2023-04-25 22:30:59 -0400

    Update shell.h

--- shell.h
+++ shell.h
@@ -23,7 +23,7 @@ extern char **environ;
 char *read_command();
 void execute_command(char **command);
 
-void split_code(char **args, int arg_count, int exit_status,char **commands);
+void split_code(char **args, int arg_count, int exit_status,char **commands, char *line);
 
 /* ------String function prototype------*/
 int _strlen(char *str);
