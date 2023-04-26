3436f8c4d56065db8aa288c60399fd30402ece7d
Marsiya Issah <masy370@gmail.com>
2023-04-25 22:27:48 -0400

    Update main.c

--- main.c
+++ main.c
@@ -27,6 +27,46 @@ args = malloc(sizeof(char *) * 64);
 arg_count = 0;
 args[arg_count++] = _strtok(command, " \t");
 
+split_code(args, arg_coun, exit_status);
+// while ((args[arg_count++] = _strtok(NULL, " \t")))
+// {
+// }
+// if (_strcmp(args[0], "exit") == 0)
+// {
+// if (args[1] != NULL)
+// {
+// exit_status = _atoi(args[1]);
+// }
+// free(line);
+// free(args);
+// free(commands);
+// exit(exit_status);
+// }
+// else if (_strcmp(args[0], "cd") == 0)
+// {
+// cd_builtin(args);
+// }
+// else
+// {
+// execute_command(args);
+// }
+free(args);
+}
+i++;
+}
+free(line);
+free(commands);
+}
+return (EXIT_SUCCESS);
+}
+
+
+
+
+
+
+void split_code(char **args, int arg_coun, int exit_status)
+{
 while ((args[arg_count++] = _strtok(NULL, " \t")))
 {
 }
@@ -49,12 +89,4 @@ else
 {
 execute_command(args);
 }
-free(args);
-}
-i++;
-}
-free(line);
-free(commands);
-}
-return (EXIT_SUCCESS);
 }
